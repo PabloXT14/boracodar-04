c201c6cd929600517ba28c6b3751c46a2a2b08c5
Pablo Alan <pabloxt14@gmail.com>
2024-05-14 20:00:00 -0300

    chore: initial project setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,7 @@
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <h1 className="m-2 w-fit rounded bg-emerald-500 px-5 py-2.5 font-bold text-emerald-50">
+      Hello World
+    </h1>
+  )
 }
