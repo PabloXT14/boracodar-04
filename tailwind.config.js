744d5336bbce275309cf30b736569f1c66e32dc2
Pablo Alan <pabloxt14@gmail.com>
2024-05-15 20:30:00 -0300

    chore: add pallet colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],
       },
+      colors: {
+        zinc: {
+          200: '#E1E1E6',
+        },
+        primary: {
+          500: '#00B37E',
+          600: '#07847E',
+        },
+        secondary: {
+          500: '#633BBC',
+          900: '#282843',
+          950: '#1A1924',
+        },
+      },
     },
   },
   plugins: [],
