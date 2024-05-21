912e187e756f157791d132ac30ec02d81b352769
Pablo Alan <pabloxt14@gmail.com>
2024-05-21 20:40:00 -0300

    feat: styling bottom section

--- src/components/bottom-section.tsx
+++ src/components/bottom-section.tsx
@@ -1,12 +1,16 @@
-import { PiPaperPlaneRight } from 'react-icons/pi'
+import { IoSend } from 'react-icons/io5'
 
 export const BottomSection = () => {
   return (
-    <section>
-      <form>
-        <input type="text" placeholder="Digite sua mensagem" />
+    <section className="pt-6">
+      <form className="flex items-center gap-2.5 rounded-full bg-secondary-900 px-6 py-4 text-zinc-200 focus-within:outline focus-within:outline-2 focus-within:outline-white">
+        <input
+          type="text"
+          placeholder="Digite sua mensagem..."
+          className="flex-1 bg-transparent text-xs font-normal placeholder:text-zinc-200"
+        />
         <button>
-          <PiPaperPlaneRight />
+          <IoSend className="text-2xl text-zinc-200" />
         </button>
       </form>
     </section>
